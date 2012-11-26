76183d5e03f18aa70a1aef769fd0091062421776
Chris Thunes <chris.thunes@bronto.com>
2012-11-26 11:13:03 -0500

    Add some basic documentation.

---
+++ doc/conf.py
@@ -0,0 +1,17 @@
+
+project = 'javasphinx'
+version = '0.9.7'
+release = '0.9.7'
+
+extensions = ['javasphinx']
+
+master_doc = 'index'
+copyright = u'2012, Bronto Software Inc.'
+
+pygments_style = 'tango'
+
+html_theme = 'nature'
+html_short_title = project
+html_sidebars = {'**' : ['localtoc.html', 'relations.html', 'sourcelink.html']}
+
+primary_domain = 'rst'
