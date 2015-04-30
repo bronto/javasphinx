8f92f3a378fdf175534329530b54763cc3b302ea
Chris Thunes <chris.thunes@bronto.com>
2015-04-30 10:26:09 -0400

    Change license to Apache 2.0

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+#
+# Copyright 2012-2015 Bronto Software, Inc. and contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 from setuptools import setup
 
@@ -9,11 +24,12 @@ setup(
     author_email = "cthunes@brewtab.com",
     url = "http://github.com/bronto/javasphinx",
     description = "Sphinx extension for documenting Java projects",
+    license = "Apache 2.0",
     classifiers = [
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries"
         ],
